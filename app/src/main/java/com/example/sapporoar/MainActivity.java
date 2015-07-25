d533d27b2a04f6f41f685093ec991b0b609fc853
aspire-hagio <hagio@aspire-inc.jp>
2015-07-25 16:01:12 +0900

    対象地点までの角度を表示

--- app/src/main/java/com/example/sapporoar/MainActivity.java
+++ app/src/main/java/com/example/sapporoar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sapporoar;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,13 +14,20 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.opengl.Matrix;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +39,16 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
     private SensorManager manager;
     private float x, y, z;
     private List<Landmark> landmarks;
+//    private double currentLatitude = 43.068084;
+//    private double currentLongitude = 141.350601;
+//    private Landmark targetLandmark = new Landmark("札幌ドーム", 43.015952, 141.409529);
+    int sx = 0;
+    int sy = 0;
+    private double currentLatitude = 43.067561;
+    private double currentLongitude = 141.498406;
+    private Landmark targetLandmark = new Landmark("札幌ドーム", 43.072726, 141.497290);
+    private List<Place> places;
+    private boolean gpsFlag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +79,48 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
         landmarks.add(new Landmark("札幌ドーム", 43.015952, 141.409529));
         landmarks.add(new Landmark("テレビ塔", 43.061297, 141.356426));
         landmarks.add(new Landmark("プリンスホテル", 43.055921, 141.341151));
+        landmarks.add(new Landmark("ローソン", 43.056440, 141.341220));
+
+        places = new ArrayList<>();
+        places.add(new Place("JRタワー", 43.068084, 141.350601));
+        places.add(new Place("aspire", 43.055688, 141.342201));
+        places.add(new Place("35", 43.067561, 141.498406));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ListView lv = new ListView(this);
+        List<String> array = new ArrayList<>();
+        for (Place place : places) {
+            array.add(place.getName());
+        }
+        array.add("GPSから取得");
+        String[] items = array.toArray(new String[0]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lv.setAdapter(adapter);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setTitle("現在地を選択してください").setView(lv);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                alertDialog.dismiss();
+                if (position < places.size()) {
+                    Place place = places.get(position);
+                    Toast.makeText(MainActivity.this, place.getName(), Toast.LENGTH_LONG).show();
+                    currentLatitude = place.getLatitude();
+                    currentLongitude = place.getLongitude();
+                    gpsFlag = false;
+                } else {
+                    // GPSから取得
+                    Toast.makeText(MainActivity.this, "GPSから取得" +
+                            "", Toast.LENGTH_LONG).show();
+                    gpsFlag = true;
+                }
+            }
+        });
     }
 
     @Override
@@ -89,6 +149,10 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
     public void onLocationChanged(Location location) {
         TextView tv_provider = (TextView) findViewById(R.id.text1);
         tv_provider.setText(location.getLatitude() + ", " + location.getLongitude());
+        if (gpsFlag) {
+            currentLatitude = location.getLatitude();
+            currentLongitude = location.getLongitude();
+        }
     }
 
     @Override
@@ -166,22 +230,56 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
             float[] val = new float[3];
 
             SensorManager.getRotationMatrix(R1, I, _aVal, _mVal);
-            SensorManager.remapCoordinateSystem(R1, SensorManager.AXIS_X, SensorManager.AXIS_Z, R2);
+            SensorManager.remapCoordinateSystem(R1, SensorManager.AXIS_Y, SensorManager.AXIS_Z, R2);
             SensorManager.getOrientation(R2, val);
             //ラジアンを角度に変換
             for (int i = 0; i < 3; i++) {
                 val[i] = (float) (val[i] * 180 / Math.PI);
             }
-
+            sb.append(String.format("lat:%f\n", currentLatitude));
+            sb.append(String.format("lng:%f\n", currentLongitude));
             sb.append(String.format("磁気センサー + 加速度センサー\n"));
             sb.append(String.format("方位角%f\n", (val[0] < 0) ? val[0] + 360 : val[0]));
             sb.append(String.format("傾斜角%f\n", val[1]));
             sb.append(String.format("回転角%f\n", val[2]));
+
+            float[] result = new float[4];
+            float[] v1 = new float[]{0, 1, 0, 0};
+            Matrix.multiplyMV(result, 0, R2, 0, v1, 0);
+
+            //  対象地点ベクトル
+//            float[] targetV = new float[]{(float) (targetLandmark.getLatitude() - currentLatitude), (float) (targetLandmark.getLongitude() - currentLongitude), 0, 0};
+            float[] targetV = new float[]{(float) (targetLandmark.getLatitude() - currentLatitude), 0, (float) (targetLandmark.getLongitude() - currentLongitude), 0};
+            float[] c = new float[4];
+            float r = (float) (getR(result, targetV) * 180 / Math.PI);
+            float n = 0;
+            for (int i = 0; i < 4; i++) {
+                n += targetV[i] * result[i];
+            }
+            sb.append(String.format("%f\n", r));
+//            sb.append(String.format("%f\n", targetV[0]));
+//            sb.append(String.format("%f\n", targetV[1]));
+//            sb.append(String.format("%f\n", targetV[2]));
+//            sb.append(String.format("%f\n", targetV[3]));
+
+//            sb.append(String.format("%f\n", c[0]));
+//            sb.append(String.format("%f\n", c[1]));
+//            sb.append(String.format("%f\n", c[2]));
+//            sb.append(String.format("%f\n", c[3]));
+
+//            sb.append(String.format("%f\n", result[0]));
+//            sb.append(String.format("%f\n", result[1]));
+//            sb.append(String.format("%f\n", result[2]));
+//            sb.append(String.format("%f\n", result[3]));
         }
 
         TextView tv = (TextView) findViewById(R.id.text1);
         tv.setText(sb.toString());
 
+    }
+
+    static float getR(float[] v1, float[] v2) {
+        return (float) Math.acos((v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]) / (Math.sqrt(v1[0] * v1[0] + v1[1] * v1[1] + v1[2] * v1[2]) * Math.sqrt(v2[0] * v2[0] + v2[1] * v2[1] + v2[2] * v2[2])));
     }
 
     @Override
